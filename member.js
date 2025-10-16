c5f177e1740715aa91f36931f1546227f711d571
Michael White <ec2463562@edinburghcollege.ac.uk>
2025-10-16 13:57:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  // Define the skills for the member
+  const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "CSS",
+    "HTML"
+  ];
+
+  // Return the skills
+  return skills;
+}
